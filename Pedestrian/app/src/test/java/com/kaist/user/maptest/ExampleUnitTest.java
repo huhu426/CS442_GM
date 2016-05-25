c8d1dededffc5588be5c32b9b59e0abb8e805247
hyeonuchoi <specialist.hyun@gmail.com>
2016-05-25 21:06:12 +0900

    pedestrian code
    pedestrian code

---
+++ Pedestrian/app/src/test/java/com/kaist/user/maptest/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.kaist.user.maptest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
